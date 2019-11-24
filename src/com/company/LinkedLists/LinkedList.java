3be19ed9330a4b19c0c4e7a21ea9bffbdbd0c1ca
bocaralpha <alphabocar.ba@gmail.com>
2019-11-24 17:16:26 +0100

    des travaux sur les linkedinlist

--- src/com/company/LinkedLists/LinkedList.java
+++ src/com/company/LinkedLists/LinkedList.java
@@ -2,4 +2,26 @@ package com.company.LinkedLists;
 
 public class LinkedList {
 
+    private Node first;
+    private Node last;
+
+    public void addLast(int item) {
+        Node node = new Node(item);
+        if (first == null) {
+            last = first = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    private class Node {
+
+        private int value;
+        private Node next;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
 }
--- src/com/company/LinkedLists/Main.java
+++ src/com/company/LinkedLists/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-
+        linkedList.addLast(3);
+        linkedList.addLast(4);
     }
 }
