3da225be581e9d13a48e9d7aac21301ac39e0fb5
bocaralpha <alphabocar.ba@gmail.com>
2019-11-24 13:55:28 +0100

    ajout d'une methode print

--- src/com/company/Arrays/Array.java
+++ src/com/company/Arrays/Array.java
@@ -3,13 +3,19 @@ package com.company.Arrays;
 import java.util.Arrays;
 
 public class Array {
+    int[] numbers = new int[3];
 
     public String createList() {
-     int [] numbers = new int [3];
-     numbers[0] = 0;
-     numbers[1] = 1;
-     numbers[2] = 2;
-     return Arrays.toString(numbers);
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        return Arrays.toString(numbers);
+    }
+
+    public void print() {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
 
