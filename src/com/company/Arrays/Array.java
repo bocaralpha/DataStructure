7a09dafff2e9145b33f78195a98b787fbdb5c46e
bocaralpha <alphabocar.ba@gmail.com>
2019-11-24 16:26:52 +0100

    ajout des methode remove et insert

--- src/com/company/Arrays/Array.java
+++ src/com/company/Arrays/Array.java
@@ -3,19 +3,48 @@ package com.company.Arrays;
 import java.util.Arrays;
 
 public class Array {
-    int[] numbers = new int[3];
+    private int[] numbers;
+    private int count;
+
+    public Array(int length) {
+        this.numbers = new int[length];
+    }
+
 
     public String createList() {
         numbers[0] = 0;
         numbers[1] = 1;
         numbers[2] = 2;
+        System.out.println(Arrays.toString(numbers));
         return Arrays.toString(numbers);
     }
 
     public void print() {
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < count; i++) {
             System.out.println(numbers[i]);
         }
+    }
+
+    public void insert(int number) {
+        if (numbers.length == count) {
+            int[] newNumbers = new int[count * 2];
+            for (int i = 0; i < count; i++) {
+                newNumbers[i] = numbers[i];
+            }
+            numbers = newNumbers;
+        }
+        numbers[count++] = number;
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index > count) {
+          throw new IllegalArgumentException();
+        }
+        // faire reculer les index a partir de cet index vers la fin de la liste
+        for (int i = index; i < count; i++)  {
+            numbers[i] = numbers[i + 1];
+        }
+        count --;
     }
 }
 
