adb1cce2289814c9d983ae1d2f8e37800f0780f0
bocaralpha <alphabocar.ba@gmail.com>
2020-02-09 11:51:46 +0100

    des travaux sur les hashmaps

--- src/com/company/Stacks/Main.java
+++ src/com/company/Stacks/Main.java
@@ -2,7 +2,7 @@ package com.company.Stacks;
 
 public class Main {
     public static void main(String[] args) {
-        boolean result = new Stack().isBalanced("{1 + 1}");
+        boolean result = new Stack().isBalanced("<1 + 1>>");
         System.out.print(result);
     }
 }
--- src/com/company/Stacks/Stack.java
+++ src/com/company/Stacks/Stack.java
@@ -5,10 +5,11 @@ public class Stack {
     public boolean isBalanced(String str) {
         var stack = new java.util.Stack<Character>();
         for (Character ch: str.toCharArray()) {
-            if (ch.equals('{')) {
+            if (ch.equals('{')  || ch.equals('(') || ch.equals('<')  ) {
                 stack.push(ch);
             }
-            if (ch.equals('}')) {
+            if (ch.equals('}') || ch.equals(')') || ch.equals('>')) {
+                if (stack.isEmpty()) { return  false; }
                 stack.pop();
             }
         }
