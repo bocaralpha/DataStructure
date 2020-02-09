4efe2d0581621fd3d7a43edea553b8f1f26b6fa3
bocaralpha <alphabocar.ba@gmail.com>
2020-02-09 11:41:09 +0100

    des travaux sur les hashmaps

---
+++ src/com/company/Stacks/Main.java
@@ -0,0 +1,8 @@
+package com.company.Stacks;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean result = new Stack().isBalanced("{1 + 1}");
+        System.out.print(result);
+    }
+}
--- src/com/company/Stacks/Stack.java
+++ src/com/company/Stacks/Stack.java
@@ -1,4 +1,17 @@
 package com.company.Stacks;
 
 public class Stack {
+
+    public boolean isBalanced(String str) {
+        var stack = new java.util.Stack<Character>();
+        for (Character ch: str.toCharArray()) {
+            if (ch.equals('{')) {
+                stack.push(ch);
+            }
+            if (ch.equals('}')) {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
 }
