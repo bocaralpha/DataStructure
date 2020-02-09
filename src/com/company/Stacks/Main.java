282cc264538b61c408cbe37feb418cc91d19d723
bocaralpha <alphabocar.ba@gmail.com>
2020-02-09 12:43:34 +0100

    des travaux sur les hashmaps

--- src/com/company/Stacks/Main.java
+++ src/com/company/Stacks/Main.java
@@ -1,8 +1,19 @@
 package com.company.Stacks;
 
+import com.company.Arrays.Array;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         boolean result = new Stack().isBalanced("<1 + 1>>");
         System.out.print(result);
+        var stack = new Stack();
+        stack.push(20);
+        stack.push(30);
+        stack.push(30);
+        stack.push(30);
+        stack.pop();
+        System.out.print(stack.toString());
     }
 }
--- src/com/company/Stacks/Stack.java
+++ src/com/company/Stacks/Stack.java
@@ -1,6 +1,13 @@
 package com.company.Stacks;
 
+import com.company.Arrays.Array;
+
+import java.util.Arrays;
+
 public class Stack {
+
+    private int[] items = new int [5] ;
+    private int count = 0;
 
     public boolean isBalanced(String str) {
         var stack = new java.util.Stack<Character>();
@@ -14,5 +21,20 @@ public class Stack {
             }
         }
         return stack.isEmpty();
+    }
+
+    public void push(int item) {
+        if (items.length < count) {
+            throw new StackOverflowError("Shit happen");
+        }
+        items[count] = item;
+        count ++;
+    }
+    public int pop() {
+        return  items[count --];
+    }
+
+    public String toString() {
+       return Arrays.toString(items);
     }
 }
